15b10d74fa85041b8895a61ba53d6f8078337030
David Ortiz <davidortizfrau@gmail.com>
2014-02-11 11:35:36 -0700

    Blog Post

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,17 @@ PortfolioSite::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.default_url_options = { :host => ENV["DEFAULT_URL"] }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "davidortizfrau.me",
+    :user_name            => ENV["GMAIL_USERNAME"],
+    :password             => ENV["GMAIL_PASSWORD"],
+    :authentication       => "plain"
+  }
 end
